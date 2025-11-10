8608ff2953ec6de1fc19df0320741477a5a3fba3
JulyMats <july.matsukevich@mail.ru>
2025-11-10 01:38:37 +0100

    feat: homeworkAssignment model impl

---
+++ src/main/java/com/app/umkaSchool/model/HomeworkAssignmentStudentGroupId.java
@@ -0,0 +1,19 @@
+package com.app.umkaSchool.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Embeddable
+public class HomeworkAssignmentStudentGroupId implements Serializable {
+    @Column(name = "homework_assignment_id")
+    private UUID homeworkAssignmentId;
+
+    @Column(name = "student_group_id")
+    private UUID studentGroupId;
+}
+
