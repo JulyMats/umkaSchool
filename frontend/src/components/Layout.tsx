ec4f0a4cf7766563442bbf2076ba958dc8c8d843
JulyMats <july.matsukevich@mail.ru>
2025-11-16 23:20:19 +0100

    fix: remove white borders and unify background color

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -9,9 +9,9 @@ interface LayoutProps {
 
 export default function Layout({ children, title, subtitle }: LayoutProps) {
   return (
-    <div className="p-6">
+    <div className="p-4 sm:p-6">
       <Header title={title} subtitle={subtitle} />
-      <div className="mt-6">
+      <div className="mt-4 sm:mt-6">
         {children}
       </div>
     </div>
