3ee3d39e8031bcd5b754e05f79d08bd8130e7c5a
JulyMats <july.matsukevich@mail.ru>
2025-11-30 23:02:31 +0100

    refactor: improve query formatting

--- src/main/java/com/app/umkaSchool/repository/StudentRepository.java
+++ src/main/java/com/app/umkaSchool/repository/StudentRepository.java
@@ -19,10 +19,14 @@ public interface StudentRepository extends JpaRepository<Student, UUID> {
 
     List<Student> findByGroup_Id(UUID groupId);
 
-    @Query("SELECT s FROM Student s WHERE s.teacher.id = :teacherId ORDER BY s.lastActivityAt DESC")
+    @Query("SELECT s FROM Student s " +
+           "WHERE s.teacher.id = :teacherId " +
+           "ORDER BY s.lastActivityAt DESC")
     List<Student> findByTeacherIdOrderByLastActivityDesc(UUID teacherId);
 
-    @Query("SELECT s FROM Student s WHERE s.group.id = :groupId ORDER BY s.user.lastName, s.user.firstName")
+    @Query("SELECT s FROM Student s " +
+           "WHERE s.group.id = :groupId " +
+           "ORDER BY s.user.lastName, s.user.firstName")
     List<Student> findByGroupIdOrderByName(UUID groupId);
 
     boolean existsByUser_Email(String email);
