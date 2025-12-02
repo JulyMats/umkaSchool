d5d875c13a42b24646167cde3b9a443a4867e230
JulyMats <july.matsukevich@mail.ru>
2025-12-03 00:32:08 +0100

    feat: add AvatarSelector, Card, PasswordField, and ToggleSwitch components for enhanced UI functionality

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,5 +1,9 @@
 export { default as Button } from './Button';
 export { default as Input } from './Input';
+export { default as PasswordField } from './PasswordField';
 export { default as LoadingSpinner } from './LoadingSpinner';
 export { default as Modal } from './Modal';
+export { default as AvatarSelector } from './AvatarSelector';
+export { default as Card } from './Card';
+export { default as ToggleSwitch } from './ToggleSwitch';
 
