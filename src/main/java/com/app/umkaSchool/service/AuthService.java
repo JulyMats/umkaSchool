68d7f996a6b3f1ed335074771fe65d4d6f0edcea
JulyMats <july.matsukevich@mail.ru>
2025-12-08 20:11:25 +0100

    feat: implement refresh token endpoint and update logout method to accept refresh token

--- src/main/java/com/app/umkaSchool/service/AuthService.java
+++ src/main/java/com/app/umkaSchool/service/AuthService.java
@@ -8,7 +8,8 @@ import com.app.umkaSchool.dto.auth.SignupResponse;
 public interface AuthService {
     SignupResponse signup(RegisterRequest request);
     LoginResponse signin(LoginRequest request);
+    LoginResponse refreshToken(String refreshToken);
     void forgotPassword(String email, String appBaseUrl);
     void resetPassword(String token, String newPassword);
-    void logout(String token);
+    void logout(String refreshToken);
 }
