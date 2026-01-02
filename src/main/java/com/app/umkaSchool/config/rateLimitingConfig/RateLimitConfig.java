2f415bac1a5d7c5a55df9632f465fe285dba6c5b
JulyMats <july.matsukevich@mail.ru>
2026-01-02 22:27:44 +0100

    fix: update bucket4j imports to use the correct package path

--- src/main/java/com/app/umkaSchool/config/rateLimitingConfig/RateLimitConfig.java
+++ src/main/java/com/app/umkaSchool/config/rateLimitingConfig/RateLimitConfig.java
@@ -1,8 +1,8 @@
 package com.app.umkaSchool.config.rateLimitingConfig;
 
-import com.bucket4j.Bandwidth;
-import com.bucket4j.Bucket;
-import com.bucket4j.Refill;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
